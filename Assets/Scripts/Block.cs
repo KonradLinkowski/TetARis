993355706f890cd892ff9db40f6ca0fe62407862
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-12-12 22:35:53 +0100

    Add block class

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TetARis.Core {
+
+	[ExecuteInEditMode]
+	public class Block : MonoBehaviour {
+
+		public GameObject[] chunks;
+		public Transform pivot;
+
+		void OnEnable() {
+			List<GameObject> chunks = new List<GameObject>();
+			foreach (Transform child in transform) {
+				if (child.CompareTag("Pivot")) {
+					pivot = child;
+				} else if (child.CompareTag("Chunk")) {
+					chunks.Add(child.gameObject);
+				}
+			}
+			this.chunks = chunks.ToArray();
+		}
+	}
+}

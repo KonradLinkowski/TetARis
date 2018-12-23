f4d10243bf46f4d09711c84a99a448c6d71aa998
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-12-23 01:59:22 +0100

    Added various logs

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+public static class Utils
+{
+  public static void Log(params object[] values) {
+    List<object> objects = new List<object>(values);
+    Debug.Log(string.Join(" ", objects.ConvertAll(o => o.ToString()).ToArray()));
+  }
+  public static void LogLines(params object[] values) {
+    List<object> objects = new List<object>(values);
+    Debug.Log(string.Join("\n", objects.ConvertAll(o => o.ToString()).ToArray()));
+  }
+
+  public static void LogBoard(bool[,] array, string trueChar, string falseChar) {
+    List<string> lines = new List<string>();
+    for (int i = array.GetLength(1) - 1; i >= 0; i--) {
+      List<string> cells = new List<string>();
+      for (int j = 0; j < array.GetLength(0); j++) {
+        cells.Add(array[j, i] ? trueChar : falseChar);
+      }
+      lines.Add(string.Join(" ", cells.ToArray()));
+    }
+    Debug.Log(string.Join("\n", lines.ToArray()));
+  }
+}

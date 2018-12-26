77ff4e7be1f9bede34f5a5655f24b3fe85a2aa78
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-12-26 15:48:39 +0100

    More android support and mouse input

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 namespace TetARis.Core {
   public class GameInput : MonoBehaviour
   {
+    private bool clicked = false;
+    private Vector2 lastMousePosition;
+
+    [Range(0, 90)]
+    [SerializeField]
+    private float angleRange;
+
+    [SerializeField]
+    private float minSwipeLength;
+    
     void Update()
     {
+      // keyboard input
       if (Input.GetKeyDown(KeyCode.W)) {
         BoardManager.Instance.Rotate(false);
       } else if (Input.GetKeyDown(KeyCode.A)) {
@@ -21,6 +32,41 @@ namespace TetARis.Core {
         BoardManager.Instance.SwapWithStash();
       } else if (Input.GetKeyDown(KeyCode.P)) {
         BoardManager.Instance.PrintBoard();
+      }
+
+      if (Input.GetMouseButtonDown(0)) {
+        clicked = true;
+        lastMousePosition = Input.mousePosition;
+      } else if (Input.GetMouseButtonUp(0)) {
+        clicked = false;
+        Vector2 difference = (Vector2)Input.mousePosition - lastMousePosition;
+        handleSwipe(difference);
+      }
+    }
+    
+    private void handleSwipe(Vector2 vector) {
+      // Single click
+      if (vector.magnitude < minSwipeLength) {
+        BoardManager.Instance.Rotate(false);
+        return;
+      }
+      // Swipes
+      float angle = Vector2.SignedAngle(Vector2.up, vector);
+      // Left
+      if (angle > 90 - angleRange && angle < 90 + angleRange) {
+        BoardManager.Instance.Move(Vector2.left);
+      }
+      // Right
+      else if (angle > -90 - angleRange && angle < -90 + angleRange) {
+        BoardManager.Instance.Move(Vector2.right);
+      }
+      // Up
+      else if (angle <= 90 - angleRange && angle >= -90 + angleRange) {
+        BoardManager.Instance.SwapWithStash();
+      }
+      // Down
+      else if (angle >= 90 + angleRange || angle <= -90 - angleRange) {
+        BoardManager.Instance.Drop();
       }
     }
   }
